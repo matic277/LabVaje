a16e112d84d773638890997095ecc59bff6b6299
matic277 <matic277@gmail.com>
2018-12-02 11:25:10 +0100

    adding instructions for some exercises

--- SQL Vaje/vaje_15.11.2018_Joins.sql
+++ SQL Vaje/vaje_15.11.2018_Joins.sql
@@ -1,16 +1,34 @@
--- 1 naloga
+
+/*
+	1 naloga
+	The HumanResources.Employee table does not contain the employee names.
+	Join that table to the Person.Person table on the BusinessEntityID column.
+	Display the job title, birth date, first name, and last name.
+*/
 select	hre.JobTitle, hre.BirthDate, pp.FirstName, pp.LastName
 from	HumanResources.Employee as hre
 		join Person.Person as pp
 		on hre.BusinessEntityID=pp.BusinessEntityID
 
--- 2 naloga
+/*
+	2 naloga
+	The customer names also appear in the Person.Person table.
+	Join the Sales.Customer table to the Person.Person table.
+	The BusinessEntityID column in the Person.Person table matches the
+	PersonID column in the Sales.Customer table. Display the CustomerID, StoreID
+	and TerritoryID columns along with the name columns.
+*/
 select	CustomerID, StoreID, TerritoryID
 from	sales.Customer as sc
 		join person.Person as pp
 		on sc.PersonID=pp.BusinessEntityID
 
--- 3 naloga
+/*
+	3 naloga
+	Extend the query written in question 2 to include the Sales.SalesOrderHeader table.
+	Display the SalesOrderID column along with the columns already specified.
+	The Sales.SalesOrderHeader table joins the Sales.Customer table on CustomerID.
+*/
 select	sc.CustomerID, StoreID, sc.TerritoryID, ss.SalesOrderID
 from	sales.Customer as sc
 		join person.Person as pp
@@ -18,7 +36,11 @@ from	sales.Customer as sc
 		join Sales.SalesOrderHeader as ss
 		on ss.CustomerID=sc.CustomerID
 
--- 4 naloga
+/*
+	4 naloga
+	Write a query that joins the Sales.SalesOrderHeader table to the Sales.SalesPerson table.
+	Join the BusinessEntityID column from the Sales.SalesPerson table to theSalesPersonID column in the Sales.
+*/
 select	*
 from	sales.SalesOrderHeader as ss
 		join sales.SalesPerson as ssp
@@ -26,33 +48,57 @@ from	sales.SalesOrderHeader as ss
 
 -- -- --
 
--- 1 naloga
+/*
+	1 naloga
+	Write a query that displays all the products along with the SalesOrderID even if
+	an order has never been placed for that product. Join to the Sales.SalesOrderDetail
+	table using the ProductID column.
+*/
 select	pp.ProductID, pp.Name, so.ProductID, so.OrderQty
 from	Production.Product as pp
 		left join sales.SalesOrderDetail as so
 		on so.ProductID=pp.ProductID
 
--- 2 naloga
+/*
+	2 naloga
+	Change the query written in step 1 so that only products that have
+	not been ordered show up in the query.
+*/
 select	pp.ProductID, pp.Name, so.ProductID, so.OrderQty
 from	Production.Product as pp
 		join sales.SalesOrderDetail as so
 		on so.ProductID=pp.ProductID
 
--- 3 naloga
+/*
+	3 naloga
+	Write a query that returns all the rows from the Sales.SalesPerson table joined
+	to the Sales.SalesOrderHeader table along with the SalesOrderID column even if
+	no orders match. Include the SalesPersonID and SalesYTD columns in the results.
+*/
 select	soh.SalesPersonID, ss.SalesYTD
 from	sales.SalesPerson as ss
 		left join sales.SalesOrderHeader soh
 		on soh.SalesPersonID=ss.BusinessEntityID
 
--- 4 naloga
+/*
+	4 naloga
+	Change the query written in question 3 so that the salesperson’s name also displays
+	from the Person.Person table.
+
+*/
 select	soh.SalesPersonID, ss.SalesYTD, pp.FirstName
 from	sales.SalesPerson as ss
 		left join sales.SalesOrderHeader soh
 		on soh.SalesPersonID=ss.BusinessEntityID
 		inner join Person.Person as pp
 		on pp.BusinessEntityID=ss.BusinessEntityID
-
--- 5 naloga
+/*
+	5 naloga
+	The Sales.SalesOrderHeader table contains foreign keys to the Sales.CurrencyRate and
+	Purchasing.ShipMethod tables. Write a query joining all three tables, and make sure it
+	contains all rows from Sales.SalesOrderHeader. Include the CurrencyRateID, AverageRate,
+	SalesOrderID, and ShipBase columns.
+*/
 select	cr.CurrencyRateID, cr.AverageRate, soh.SalesOrderID, sm.ShipBase
 from	sales.SalesOrderHeader as soh
 		left join sales.CurrencyRate as cr
--- SQL Vaje/vaje_24.10.2018.sql
+++ SQL Vaje/vaje_24.10.2018.sql
@@ -1,81 +1,103 @@
-/*
 -- 1 naloga
-SELECT businessEntityID, LoginID, JobTitle
-FROM HumanResources.Employee
-WHERE  JobTitle = 'Research and Development Engineer'
+-- Write a query using a WHERE clause that displays all the employees listed in the
+-- HumanResources.Employee table who have the job title Research and
+-- Development Engineer. Display the business entity ID number, the login ID, and the title for each one
+SELECT	businessEntityID, LoginID, JobTitle
+FROM	HumanResources.Employee
+WHERE 	JobTitle = 'Research and Development Engineer'
 
 -- 2 naloga
-SELECT [FirstName], [MiddleName], [LastName], [BusinessEntityID]
-FROM Person.Person
-WHERE MiddleName = 'J.'
+-- Write a query using a WHERE clause that displays all the names in Person.Person
+-- with the middle name J. Display the first, last, and middle names along with the ID
+-- numbers.
+SELECT	[FirstName], [MiddleName], [LastName], [BusinessEntityID]
+FROM	Person.Person
+WHERE	MiddleName = 'J.'
 
 -- 3 naloga
-SELECT businessEntityID, LoginID, JobTitle
-FROM HumanResources.Employee
-WHERE  JobTitle != 'Research and Development Engineer'
-*/
+-- Rewrite the query you wrote in question 1, changing it so that the employees who
+-- do not have the title Research and Development Engineer are displayed
+SELECT	businessEntityID, LoginID, JobTitle
+FROM	HumanResources.Employee
+WHERE	JobTitle != 'Research and Development Engineer'
+
 -----------------------------------------------------------
-/*
+
 -- 1 naloga
-select [ProductID], [Name]
-from Production.Product
+-- Write a query that displays the product ID and name for each product from the
+-- Production.Product table with a name starting with Chain
+select	[ProductID], [Name]
+from	Production.Product
 where name like 'CHAIN%'
 
 -- 2 naloga
-select [ProductID], [Name]
-from Production.Product
+-- Write a query like the one in question 1 that displays the products with Paint in   the name
+select	[ProductID], [Name]
+from	Production.Product
 where name like '%paint%'
 
 -- 3 naloga
-select [ProductID], [Name]
-from Production.Product
+-- Change the last query so that the products without Paint in the name are displayed
+select	[ProductID], [Name]
+from	Production.Product
 where name not like '%paint%'
-*/
+
 -----------------------------------------------------------
-/*
+
 -- 1 naloga
+-- Write a query displaying the ProductID, Name, and Color columns from rows in the Production.Product table.
+-- Display only those rows where no color has been assigned
 SELECT	ProductID, Name, Color
 FROM	Production.Product
 WHERE	color IS NULL
 
 -- 2 naloga
+-- Write a query displaying the ProductID, Name, and Color columns from rows in the Production.Product table.
+-- Display only those rows in which the color is known not to be blue.
 SELECT	ProductID, Name, Color
 FROM	Production.Product
 WHERE	color != 'blue' AND color IS NOT NULL
 
 -- 3 naloga
+-- Write a query displaying ProductID, Name, Style, Size, and Color from the
+-- Production.Product table. Include only the rows where at least one of the Style, Size, or 
+-- Color columns contains a value
 SELECT	ProductID, Style, SIZE, Color
 FROM	Production.Product
 WHERE	color IS NOT NULL OR
 		style IS NOT NULL OR
 		SIZE  IS NOT NULL
-*/
 
-/*
+
 select	*
 from	Production.ProductReview
 where	contains (Comments, 'overall')
-*/
+
 
 -----------------------------------------------------------
-/*
+
 -- 1 naloga
+-- Write a query that returns the business entity ID and name columns from the Person.Person table.
+-- Sort the results by LastName, FirstName, and MiddleName.
+
 select	BusinessEntityID, FirstName, MiddleName, LastName
 from	person.person
 order by FirstName, MiddleName, LastName
 
 -- 2 naloga
+-- Modify the query written in question 1 so that the data is returned in the opposite order
 select	BusinessEntityID, FirstName, MiddleName, LastName
 from	person.person
 order by FirstName desc, MiddleName desc, LastName desc
 
 -- 3 naloga
+-- Modify the query written in question 1 so that you return only 10 rows starting at row 20
 select	BusinessEntityID, FirstName, MiddleName, LastName
 from	person.person
 order by FirstName, MiddleName, LastName
 offset 19 rows
 fetch next 10 rows only
-*/
+*
 
 -- prva ne null vrednost
 select coalesce(null, 1, null, 2) as value
