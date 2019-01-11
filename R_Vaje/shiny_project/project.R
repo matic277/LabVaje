94666d81c17d56a72b97c7034714c7f41942a710
matic277 <matic277@gmail.com>
2019-01-11 22:36:43 +0100

    data

--- R_Vaje/shiny_project/project.R
+++ R_Vaje/shiny_project/project.R
@@ -2,7 +2,8 @@ library("rio")
 library("dplyr")
 library("ggplot2")
 
-sourceDir = "C:/git/SQL-R/LabVaje/R_Vaje/data_resources/"
+
+sourceDir = "C:/git/LabVaje/R_Vaje/data_resources/"
 fileName1 = "googleplaystore.csv"
 fileName2 = "googleplaystore_user_reviews.csv"
 
@@ -11,23 +12,38 @@ filePath2 = paste(sourceDir, fileName2, sep="");
 
 data = rio::import(file=filePath1, format="csv")
 
-
 colnames(data)
 
-udf <- function(d) {
-  tmp = 0
-  sum = 0
-  for (v in d) {
-    if (startsWith(v, "$")) tmp = as.numeric(substr(v, 2, nchar(v)))
-    else tmp = as.numeric(v)
-    sum = sum + tmp
-  }
-  print(class(d))
+tibble::as.tibble(data)
 
-  return(1);
+covertPrice <- function(d) {
+  ifelse (startsWith(d, "$"), as.numeric(substr(d, 2, nchar(d))), as.numeric(d))
 }
 
-data = data %>% mutate("Price2"=udf(Price))
+# fix prices to integers
+data = data %>% mutate("ePrice"=covertPrice(Price))
 
-data %>% select(Size, Price) %>% filter(Size=="Varies with device") %>% group_by(Size) %>% summarise(udf(Price))
+# fix sizes to megabytes, 'Varies with device' = -1
+convertSize <- function(d) {
+  if (endsWith(d, "k")) return(as.numeric(substr(d, 1, nchar(d)-1)) / 1000)
+  else if (endsWith(d, "M")) return(as.numeric(substr(d, 1, nchar(d)-1)))
+  else return(-1)
+}
 
+data = data %>% mutate("eSize"=convertSize(Size))
+data[is.na(data)] <- -1 # replace NA values with -1, because function doest work for some reason
+
+
+# remove a row that has Price value as NaN
+data = data[!(data$Type==NaN), ]
+
+
+# fix Install values to int
+convertInstalls <- function(d) {
+  d = gsub("\\+", "", gsub(",", "", d))
+  return(as.numeric(d, options(scipen = 999)))
+}
+
+data = data %>% mutate("eInstalls"=convertInstalls(Installs))
+
+data %>% select(eInstalls, ePrice) %>% group_by(eInstalls) %>% summarise(mean(ePrice)) %>% arrange(desc(nchar(eInstalls)))
